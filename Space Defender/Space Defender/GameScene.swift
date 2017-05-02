ceba03a06efdd805fcc072bf1e211589beef4afb
Dulio Denis <dulio.denis@yahoo.com>
2017-05-02 12:24:51 -0400

    Added the space ship to the game scene

--- Space Defender/Space Defender/GameScene.swift
+++ Space Defender/Space Defender/GameScene.swift
@@ -11,7 +11,19 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    let spaceship = SKSpriteNode(imageNamed: "rocket")
+    
     override func didMove(to view: SKView) {
+        // midnight blue background
+        backgroundColor = SKColor(red: 44/256, green: 62/256, blue: 80/256, alpha: 1.0)
+        
+        // position, scale and anchor the spaceship
+        spaceship.position = CGPoint(x: size.width * 0.2, y: size.height * 0.2)
+        spaceship.setScale(0.4)
+        spaceship.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        spaceship.zPosition = 5
+        spaceship.name = "spaceship"
+        addChild(spaceship)
         
     }
     
